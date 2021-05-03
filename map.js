8d533c102631f3540672dfe734f2485092f3c216
Nebojša Draganić <nebojsa311@gmail.com>
2021-05-04 00:34:21 +0200

    small code optimization

--- map.js
+++ map.js
@@ -1,19 +1,7 @@
-let mymap = L.map("mapid").setView([8.82, 32.156], 13);
-let marker = L.marker([8.82, 32.156]).addTo(mymap);
-
-L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: "mapbox/streets-v11",
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken:
-      "pk.eyJ1IjoibmVib2pzYTMxMSIsImEiOiJja282eml5MWQxcjJjMndseWR0eWdpNHpqIn0.7CCC_DwCQM7pLUfuBnDjoA",
-  }
-).addTo(mymap);
+let mymap = L.map("mapid").setView(
+  [1.1, 1.1],
+  13
+);
 
 const URL =
   "https://geo.ipify.org/api/v1?apiKey=at_RyMzs9DPkbrvltZhi19LdhwM9MRpq&ipAddress=";
@@ -35,11 +23,16 @@ function getLocationData() {
       data.data.location.postalCode;
     document.getElementById("isp").innerHTML = data.data.isp;
     lat = data.data.location.lat;
-    lng = data.data.location.lng;
+
     mymap.off();
     mymap.remove();
-    mymap = L.map("mapid").setView([data.data.location.lat, data.data.location.lng], 13);
+    
+    mymap = L.map("mapid").setView(
+      [data.data.location.lat, data.data.location.lng],
+      13
+    );
     marker = L.marker([data.data.location.lat, data.data.location.lng]).addTo(mymap);
+
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
