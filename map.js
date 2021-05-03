d91ae9c63447fbfffb837d3d821f74d9e651a91b
Nebojša Draganić <nebojsa311@gmail.com>
2021-05-04 00:02:35 +0200

    leaflet maps update corectly depending on ip

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
-let mymap = L.map("mapid").setView([43.799, 18.34], 13);
-let marker = L.marker([43.799, 18.34]).addTo(mymap);
+let mymap = L.map("mapid").setView([8.82, 32.156], 13);
+let marker = L.marker([8.82, 32.156]).addTo(mymap);
 
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -15,10 +15,9 @@ L.tileLayer(
   }
 ).addTo(mymap);
 
-const URL = "https://geo.ipify.org/api/v1?apiKey=at_RyMzs9DPkbrvltZhi19LdhwM9MRpq&ipAddress=";
+const URL =
+  "https://geo.ipify.org/api/v1?apiKey=at_RyMzs9DPkbrvltZhi19LdhwM9MRpq&ipAddress=";
 let fullURL = null;
-let lat = null;
-let lng = null;
 
 function completeURL() {
   let inputValue = document.getElementById("ipadress").value;
@@ -27,15 +26,33 @@ function completeURL() {
 }
 
 function getLocationData() {
-  axios.get( fullURL ? fullURL : URL ).then((data) => {
+  axios.get(fullURL ? fullURL : URL).then((data) => {
     document.getElementById("yourIp").innerHTML = data.data.ip;
     document.getElementById("city").innerHTML = data.data.location.city;
-    document.getElementById("region").innerHTML= data.data.location.region;
+    document.getElementById("region").innerHTML = data.data.location.region;
     document.getElementById("timeZone").innerHTML = data.data.location.timezone;
-    document.getElementById("postalCode").innerHTML = data.data.location.postalCode;
+    document.getElementById("postalCode").innerHTML =
+      data.data.location.postalCode;
     document.getElementById("isp").innerHTML = data.data.isp;
     lat = data.data.location.lat;
     lng = data.data.location.lng;
+    mymap.off();
+    mymap.remove();
+    mymap = L.map("mapid").setView([data.data.location.lat, data.data.location.lng], 13);
+    marker = L.marker([data.data.location.lat, data.data.location.lng]).addTo(mymap);
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1IjoibmVib2pzYTMxMSIsImEiOiJja282eml5MWQxcjJjMndseWR0eWdpNHpqIn0.7CCC_DwCQM7pLUfuBnDjoA",
+      }
+    ).addTo(mymap);
   });
 }
 
@@ -44,6 +61,6 @@ document
   .getElementById("submitButton")
   .addEventListener("click", getLocationData);
 
-  window.addEventListener('load', (event) => {
-    getLocationData();
-  });
+window.addEventListener("load", (event) => {
+  getLocationData();
+});
