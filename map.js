9f53dbba6bd636ec56d5c0a77a347855e8842ee1
Nebojša Draganić <nebojsa311@gmail.com>
2021-05-05 20:29:00 +0200

    finished

--- map.js
+++ map.js
@@ -33,8 +33,6 @@ function getLocationData() {
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: "mapbox/streets-v11",
         tileSize: 512,
